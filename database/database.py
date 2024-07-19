4de5b4c97835d963c80d8b676d7428c95adf1e8d
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-19 08:55:05 +0530

    Add files via upload

---
+++ database/database.py
@@ -0,0 +1,38 @@
+# Credit @TheBlackXYZ.
+# Please Don't remove credit.
+# TheBlackXYZBotz Forever !
+# Thanks You For Help Us In This Amazing Creativity 
+# Thanks You For Giving Me Credit @TheBlackXYZBotz
+# For Any ERROR Please Contact Me -> Telegram ->@TheBlackXYZBotz & Insta @TheBlackXYZ
+# Please Love & Support 💗💗🙏
+
+import pymongo, os
+from info import DATABASE_URI, DATABASE_NAME
+
+
+dbclient = pymongo.MongoClient(DATABASE_URI)
+database = dbclient[DATABASE_NAME]
+
+
+user_data = database['users']
+
+
+async def present_user(user_id : int):
+    found = user_data.find_one({'_id': user_id})
+    return bool(found)
+
+async def add_user(user_id: int):
+    user_data.insert_one({'_id': user_id})
+    return
+
+async def full_userbase():
+    user_docs = user_data.find()
+    user_ids = []
+    for doc in user_docs:
+        user_ids.append(doc['_id'])
+        
+    return user_ids
+
+async def del_user(user_id: int):
+    user_data.delete_one({'_id': user_id})
+    return
